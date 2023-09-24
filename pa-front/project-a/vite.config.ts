9f78755cc525d60215289ffaa84ad54de6b614f8
dachikusokun <ardija.xyz@gmail.com>
2023-09-24 15:13:19 +0900

    add:Eslint,typechecker,yarn_lock

--- pa-front/project-a/vite.config.ts
+++ pa-front/project-a/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      typescript: false,
+      eslint: {
+        lintCommand: 'eslint --ext .tsx,.ts,.jsx,.js .',
+      },
+    }),
+  ],
 })
